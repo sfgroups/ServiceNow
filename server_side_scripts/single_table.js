803ce71182fa618d51b863df372332664faf6ee4
software groups <admin@sfgroups.com>
2025-07-08 17:43:56 -0400

    different query options

--- server_side_scripts/single_table.js
+++ server_side_scripts/single_table.js
@@ -13,3 +13,25 @@ gr.query();
 while (gr.next()) {
     gs.print('Matched Record: ' + gr.getDisplayValue('number') + ' | Planned Start Date: ' + gr.getDisplayValue('planned_start_date'));
 }
+//
+
+var gr = new GlideRecord('sn_dpr_model_release');
+gr.addNotNullQuery('planned_start_date');
+gr.addQuery('planned_start_date', 'ON', todayString);
+gr.addQuery('state', 'pending');
+gr.query();
+///
+var today = "2026-07-11";  // Or use gs.nowDate() for today's date
+
+var gr = new GlideRecord('sn_dpr_model_release');
+gr.addEncodedQuery("planned_start_dateON" + today + "^state=pending");
+
+gs.info("Running query for planned_start_date = " + today);
+gr.query();
+
+var count = 0;
+while (gr.next()) {
+    gs.info("Matched: " + gr.getDisplayValue('number') + " | Date: " + gr.getDisplayValue('planned_start_date'));
+    count++;
+}
+gs.info("Total matched records: " + count);
