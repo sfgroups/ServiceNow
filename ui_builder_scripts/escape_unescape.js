4029abd4c2eb62761480912f5877090af8dbacad
software groups <admin@sfgroups.com>
2025-08-07 17:11:32 -0400

    add more character handling

--- ui_builder_scripts/escape_unescape.js
+++ ui_builder_scripts/escape_unescape.js
@@ -1,18 +1,24 @@
-function escapeTextAreaValue(str) {
-  if (typeof str !== 'string') return '';
-  return str
-    .replace(/\\/g, '\\\\')     // Escape backslashes
-    .replace(/\n/g, '\\n')      // Escape newlines
-    .replace(/\r/g, '\\r')      // Escape carriage return
-    .replace(/"/g, '\\"');      // Escape double quotes
+function escapeTextAreaForWrite(text) {
+  if (typeof text !== 'string') return '';
+
+  let workingText = text
+    .slice(0, 1000)                // Limit to 1000 characters
+    .replace(/\\n/g, '\n')         // Clean double-escaped newlines
+    .replace(/\^/g, '')            // Remove caret
+    .replace(/\\/g, '\\\\')        // Escape backslashes
+    .replace(/"/g, '\\"')          // Escape double quotes
+    .replace(/'/g, "\\'")          // Escape single quotes
+    .replace(/(\r\n|\r|\n)/g, '\\n'); // Encode newlines
+
+  return workingText;
 }
 
+function unescapeTextAreaFromRead(text) {
+  if (typeof text !== 'string') return '';
 
-function unescapeTextAreaValue(str) {
-  if (typeof str !== 'string') return '';
-  return str
-    .replace(/\\n/g, '\n')
-    .replace(/\\r/g, '\r')
-    .replace(/\\"/g, '"')
-    .replace(/\\\\/g, '\\');
+  return text
+    .replace(/\\\\/g, '\\')        // Unescape backslashes
+    .replace(/\\"/g, '"')          // Unescape double quotes
+    .replace(/\\'/g, "'")          // Unescape single quotes
+    .replace(/\\n/g, '\n');        // Convert \n to actual newline
 }
